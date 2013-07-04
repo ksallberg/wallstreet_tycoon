b3bff440fbb6753041ab250fed0d8b9c6909ec21
Kristian Sällberg <kristian@purestyle.se>
2013-07-05 01:49:18 +0200

    added animated walking character support

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'djangosettings'
 import control.savesLookup
+import view.character
+from view.character import Character
 import pygame
 import math
 import datetime
@@ -15,14 +17,67 @@ from control.scheduledEvents import *
 #modelInit()
 
 pygame.init()
-screen = pygame.display.set_mode((1184, 800)) # (37*32,25*32)
+screen = pygame.display.set_mode((1184, 800))
 now = datetime.datetime.now()
 pygame.display.set_caption('Wallstreet Tycoon')
 clock = pygame.time.Clock()
 
+characters = []
+
+char = Character()
+char.x = 100
+char.y = 100
+char.setType(char.type4)
+characters.append(char)
+
+char2 = Character()
+char2.x = 200
+char2.y = 100
+char2.setType(char2.type2)
+characters.append(char2)
+
+char3 = Character()
+char3.x = 300
+char3.y = 100
+char3.setType(char3.type3)
+characters.append(char3)
+
+char4 = Character()
+char4.x = 400
+char4.y = 100
+char4.setType(char4.type4)
+characters.append(char4)
+
+char5 = Character()
+char5.x = 500
+char5.y = 100
+char5.setType(char5.type5)
+characters.append(char5)
+
+char6 = Character()
+char6.x = 600
+char6.y = 100
+char6.setType(char6.type6)
+characters.append(char6)
+
+char7 = Character()
+char7.x = 700
+char7.y = 100
+char7.setType(char7.type7)
+characters.append(char7)
+
+char8 = Character()
+char8.x = 800
+char8.y = 100
+char8.setType(char8.type8)
+characters.append(char8)
+
+charSheet = pygame.image.load('resources/characters.png')
+
 """
 map!
 """
+
 townmap = tiledtmxloader.tmxreader.TileMapParser().parse_decode('resources/town.tmx')
 
 resources = tiledtmxloader.helperspygame.ResourceLoaderPygame()
@@ -43,7 +98,7 @@ map!
 """
 
 def loadImage(sheet, indexX, indexY):
-    rect = Rect((indexX,indexY, 32, 32))
+    rect = Rect((indexX,indexY, 32, 48))
     image = Surface(rect.size, SRCALPHA)
     image.blit(sheet, (0, 0), rect)
     image.set_colorkey(-1, RLEACCEL)
@@ -52,15 +107,22 @@ def loadImage(sheet, indexX, indexY):
 def main():
    while True:
       for event in pygame.event.get():
-         if(event.type == pygame.KEYDOWN):
+         if event.type == pygame.KEYDOWN:
+            characters[0].setState(char.STATE_WALKING)
             if   event.key == pygame.K_UP and camera[1] > 0:
                camera[1] -= townmap.tileheight
+               characters[0].setDir("north")
             elif event.key == pygame.K_DOWN and camera[1] < 2400:
                camera[1] += townmap.tileheight
+               characters[0].setDir("south")
             elif event.key == pygame.K_RIGHT and camera[0] < 2016:
                camera[0] += townmap.tilewidth
+               characters[0].setDir("east")
             elif event.key == pygame.K_LEFT  and camera[0] > 0:
                camera[0] -= townmap.tilewidth
+               characters[0].setDir("west")
+         elif event.type == pygame.KEYUP:
+            characters[0].setState(char.STATE_STANDING)
       
       renderer.set_camera_position(camera[0],camera[1],'topleft')
       
@@ -80,8 +142,17 @@ def main():
       s.fill((0,255,255))
       screen.blit(s, (mouseX-aa,mouseY-bb))
       
+      # draw characters
+      for ch in characters:
+         chpos = ch.getOffset()
+         img = loadImage(charSheet,
+                         chpos[0],
+                         chpos[1]
+                        )
+         screen.blit(img, (ch.x, ch.y))
+      
       pygame.display.flip()
-      clock.tick(13)
+      clock.tick(60)
       
 if __name__ == '__main__':
    main()
---
+++ view/character.py
@@ -0,0 +1,130 @@
+class Character:
+   
+   DIR_SOUTH = "south"
+   DIR_NORTH = "north"
+   DIR_WEST  = "west"
+   DIR_EAST  = "east"
+   
+   STATE_STANDING  = "standing"
+   STATE_WALKING   = "walking"
+   
+   charWidth  = 32
+   charHeight = 48
+   
+   # These describe where each 
+   # character's different sprites
+   # are located.
+   south1 = (0,           0)
+   south2 = (1*charWidth, 0)
+   south3 = (2*charWidth, 0)
+   
+   west1  = (0,           1*charHeight)
+   west2  = (1*charWidth, 1*charHeight)
+   west3  = (2*charWidth, 1*charHeight)
+   
+   east1  = (0,           2*charHeight)
+   east2  = (1*charWidth, 2*charHeight)
+   east3  = (2*charWidth, 2*charHeight)
+   
+   north1 = (0,           3*charHeight)
+   north2 = (1*charWidth, 3*charHeight)
+   north3 = (2*charWidth, 3*charHeight)
+   
+   # distance to
+   type1  = (0,0)
+   type2  = (charWidth*3,0)
+   type3  = (charWidth*6,0)
+   type4  = (charWidth*9,0)
+   
+   type5  = (0,charHeight*4)
+   type6  = (charWidth*3,charHeight*4)
+   type7  = (charWidth*6,charHeight*4)
+   type8  = (charWidth*9,charHeight*4)
+   
+   type = None
+   
+   state = STATE_STANDING
+   direction = DIR_SOUTH
+   x = 0
+   y = 0
+   picture = 'resources/characters.py'
+   animState = 0
+   curAnim = None
+   
+   def getOffset(this):
+      
+      offset = (0,0)
+      
+      if   this.state == this.STATE_STANDING:
+         if   this.direction == this.DIR_SOUTH:
+            offset = (this.type[0]+this.south2[0],this.type[1]+this.south2[1])
+         elif this.direction == this.DIR_NORTH:
+            offset = (this.type[0]+this.north2[0],this.type[1]+this.north2[1])
+         elif this.direction == this.DIR_WEST:
+            offset = (this.type[0]+this.west2[0],this.type[1]+this.west2[1])
+         elif this.direction == this.DIR_EAST:
+            offset = (this.type[0]+this.east2[0],this.type[1]+this.east2[1])
+            
+      elif this.state == this.STATE_WALKING:
+         
+         if this.animState == 0:
+            if   this.direction == this.DIR_SOUTH:
+               this.curAnim = this.south3
+            elif this.direction == this.DIR_NORTH:
+               this.curAnim = this.north3
+            elif this.direction == this.DIR_WEST:
+               this.curAnim = this.west3
+            elif this.direction == this.DIR_EAST:
+               this.curAnim = this.east3
+         elif this.animState == 1:
+            if   this.direction == this.DIR_SOUTH:
+               this.curAnim = this.south2
+            elif this.direction == this.DIR_NORTH:
+               this.curAnim = this.north2
+            elif this.direction == this.DIR_WEST:
+               this.curAnim = this.west2
+            elif this.direction == this.DIR_EAST:
+               this.curAnim = this.east2
+         elif this.animState == 2:
+            if   this.direction == this.DIR_SOUTH:
+               this.curAnim = this.south1
+            elif this.direction == this.DIR_NORTH:
+               this.curAnim = this.north1
+            elif this.direction == this.DIR_WEST:
+               this.curAnim = this.west1
+            elif this.direction == this.DIR_EAST:
+               this.curAnim = this.east1
+         
+         if this.animState < 3:
+            this.animState += 0.5
+         else:
+            this.animState = 0
+         
+         offset = (this.type[0]+this.curAnim[0],this.type[1]+this.curAnim[1])
+         
+      return offset
+   
+   def width(this):
+      return this._width
+   
+   def offset(this):
+      return this.sprite_initial_offset
+      
+   def setOffset(this,off):
+      this.sprite_initial_offset = off
+      
+   def dist(this):
+      return this.sprite_distance
+      
+   def setDir(this,dire):
+      this.direction = dire
+            
+   def pic(this):
+      return this.picture
+   
+   def setType(this,type):
+      this.type = type
+   
+   def setState(this,state):
+      this.state = state
+      this.setDir(this.direction)
