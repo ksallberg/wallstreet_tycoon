b6adcd38e2a4689fc6b9daa4b95b06269fad2b2c
Kristian Sällberg <kristian@purestyle.se>
2013-07-14 20:56:21 +0200

    updated readme

--- README.md
+++ README.md
@@ -3,11 +3,10 @@ wallstreet_tycoon
 
 pygame implementation of a basic stock trading simulation
 
-Stuff i'm using:
-OS             = Mac OS X Version 10.7.5
-python version = Python 2.7.5 from python.org
-django version = '1.5.1'
-pygame version = '1.9.2pre'
-sqlite version = SQLite version 3.7.7 2011-06-25 16:35:41
-pyglet version = 1.1.4
-tiledtmxloader = 3.0.3.114
+This project is written using Python 2.7.5 from python.org
+
+Use this to install dependencies:
+python setup.py install
+
+to run:
+python test.py
--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
-#from distutils.core import setup
+import os
 from setuptools import setup
 
-setup(name       = 'wallstreet_tycoon',
-      version    = '0.5',
-      author     = 'Kristian Sallberg',
-      author_email = 'kristian@purestyle.se',
-      packages = [],
-      scripts = [],
-      url = 'https://github.com/ksallberg/wallstreet_tycoon',
-      license = '',
-      description = 'A stock simulation game.',
-      long_description = '',
-      install_requires = ['pygame >= 1.9.2',
+setup(name             = 'wallstreet_tycoon',
+      version          = '0.5',
+      author           = 'Kristian Sallberg',
+      author_email     = 'kristian@purestyle.se',
+      url              = 'https://github.com/ksallberg/wallstreet_tycoon',
+      description      = 'A stock simulation game.',
+      install_requires = ['Django >= 1.5.1',
                           'pyglet >= 1.1.4',
                           'tiledtmxloader >= 3.0.3.114',
-                          'Django >= 3.0.3',
+                          'pygame>=1.9.2pre',
                          ],
       
      )
