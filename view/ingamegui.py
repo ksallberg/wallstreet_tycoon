9169c4cf8380d55568ed461d251696aa9d1a4173
Kristian Sällberg <kristian@purestyle.se>
2013-07-27 03:31:44 +0200

    now possible to sell stocks from the portfolio

--- view/ingamegui.py
+++ view/ingamegui.py
@@ -190,6 +190,13 @@ class PortfolioGUI(AbstractGUI):
    
    cacheList           = None #used to optimize drawing of texts
    
+   sellStockGUI        = SellStock()
+   sellStockOpen       = False
+   sellAmount          = ''
+   buttonPressed       = 0
+   
+   stockCache          = {}
+   
    def __init__(self):
       
       self.scrollbar.x = 669
@@ -223,9 +230,58 @@ class PortfolioGUI(AbstractGUI):
       
       self.cacheList = pygame.Surface((500,((len(stocks)*(self.guiRowDist*3)))), pygame.SRCALPHA, 32)
       self.cacheList = self.cacheList.convert_alpha()
+   
+   # override to find when the user has pressed buy
+   def findPressed(self,x,y):
       
+      if self.sellStockOpen:
+         
+         if x >= 335 and x <= 335+127 and y >= 332 and y <= 332+41:
+            self.sellStockOpen = False
+         elif x >= 466 and x <= 466+127 and y >= 332 and y <= 332+41:
+            
+            #register the buy in the model
+            player                   = Investor.objects.get(type='player')
+            
+            stocks = Portfolio.objects.filter(investor=player)
+            
+            company                  = Company.objects.get(id=self.stockCache[self.buttonPressed])
+            sellStock(player,company,int(self.sellAmount))
+            self.sellStockOpen = False
+            
+      else:
+      
+         for button in self.buttons:
+         
+            if x >= 225 and x <= 225+128:
+               relativePosition = (y + (-self.scrolledContentY) + self.scrolledContentYTop - 150)%70 #the position in the entire Surface
+                                                                                  #self.scrolledContentY will be positive at first and then negative
+            
+               if relativePosition >= 0 and relativePosition <= 38:
+               
+                  #138 pixels between every button
+                  self.buttonPressed = floor(((-self.scrolledContentY)+y)/70)
+                  
+                  self.sellStockOpen = True
+                  self.sellAmount = '' # reset this pop up gui
+               
+            if (x >= button.x and x <= button.x + button.width and
+                y >= button.y and y <= button.y + button.height):
+                return button
+         
+      return None
+   
    def readEvent(self,event):
       self.scrollbar.readEvent(event)
+      
+      if self.sellStockOpen:
+         
+         # This is listening to the text field
+         if event.type == pygame.KEYDOWN:
+            if pygame.key.name(event.key) == 'backspace':
+               self.sellAmount = self.sellAmount[:-1]
+            elif pygame.key.name(event.key) in ['1','2','3','4','5','6','7','8','9','0'] and len(self.sellAmount) < 5:
+               self.sellAmount += pygame.key.name(event.key)
       
    def drawExtra(self,screen):
       
@@ -238,6 +294,11 @@ class PortfolioGUI(AbstractGUI):
       
       s = pygame.Surface((500,len(stocks)*self.guiRowDist*4), pygame.SRCALPHA, 32)
       s = s.convert_alpha()
+      
+      i = 0
+      for stock in stocks:
+         self.stockCache[i] = stock.company.id
+         i += 1
       
       i = 0
       for stock in stocks:
@@ -257,6 +318,13 @@ class PortfolioGUI(AbstractGUI):
       self.cacheList.blit(s,(0,0))
       
       screen.blit(self.cacheList, (227,self.scrolledContentYTop),Rect(0,self.scrolledContentYTop-self.scrolledContentY,415,336))
+      
+      if self.sellStockOpen:
+         screen.blit(self.sellStockGUI.image,(self.sellStockGUI.x,self.sellStockGUI.y))
+         
+         font  = pygame.font.SysFont('monospace',20)
+         label = font.render(self.sellAmount, 1, (78,49,11))
+         screen.blit(label,(408,282))
       
 class OpponentsGUI(AbstractGUI):
    
