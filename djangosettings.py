7c1779eb9b781f5d53ad1f817e12d76c35fbd085
Kristian Sällberg <kristian@purestyle.se>
2013-08-06 15:18:41 +0200

    removed wall tile which was not needed

--- djangosettings.py
+++ djangosettings.py
@@ -2,7 +2,7 @@ SECRET_KEY = '!'
 DATABASES = {
     'default': {
         'ENGINE':   'django.db.backends.sqlite3',
-        'NAME':     'saves/2013-08-05-20-00-29.save'
+        'NAME':     'saves/2013-08-05-20-15-31.save'
     }
 }
 INSTALLED_APPS = (
